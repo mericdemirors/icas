ea2c9273f96faa1a2e123ef667b2d22114c42695
mericdemirors <mericdemirors218@gmail.com>
2024-04-18 16:02:27 +0300

    updated test code

--- clusterimg/README.md
+++ clusterimg/README.md
@@ -5,22 +5,19 @@ Tool for clustering mixed images
 TO-TRY
 
 TO-ADD
-- test code
 - openmp
 - C/C++/CUDA optimization
 - Machine and Deep Learning
+- delete randomness
 
 TO-TIDY
-- move thread functions in another file
-- move image reading functions to another file
-- move global variables in another file
-- functionalize multiple codes
-- comment the whole code line by line
-- try to remove images_folder_path complately
 - variable names and full path list computations on outer functions
 - clean the inside of process
+- try to remove images_folder_path and destination container folder parameters in functions complately
+- move thread functions in another file
+- move image reading functions to another file
+- functionalize multiple codes
 
 TO-FIX
+- delete unnecessary variables/computations
 - fix interactive threshold selection
-- merge get_image_dict and get_template_dict in one and merge get_corner_features, delete unnecessary variables/computations
-- inside of select_threshold function
--- clusterimg/test.py
+++ clusterimg/test.py
@@ -5,18 +5,22 @@ from helper_functions import generate_test_dataset
 from clustering import Clustering
 
 test_path = os.path.join(os.path.split(os.path.dirname(os.path.abspath(__file__)))[0], "test_images")
-generate_test_dataset(test_path, 1000)
 
-clstr = Clustering(images_folder_path=test_path, method="minhash", num_of_threads=8, threshold=0.125,
-                   batch_size=50, overwrite="Y", transfer="copy", verbose=6, size=(100,100), option="dontmerge")
-print(clstr)
-clstr()
+for method in ["SSIM", "TM", "imagehash", "minhash", "ORB"]:
+    for transfer in ["copy", "move"]:
+        # generate test dataset
+        generate_test_dataset(test_path, 100)
 
-clstr = Clustering(images_folder_path=test_path + "_clustered", method="minhash", num_of_threads=8, threshold=0.125,
-                   batch_size=50, overwrite="Y", transfer="copy", verbose=6, scale=(1,1), option="merge")
-print(clstr)
-clstr()
+        # create first Clustering object to cluster but dont merge test images
+        clstr1 = Clustering(images_folder_path=test_path, method=method, num_of_threads=8, threshold=0.125,
+                        batch_size=50, overwrite="Y", transfer=transfer, verbose=1, size=(100,100), option="dontmerge")
+        clstr1()
 
-shutil.rmtree(test_path)
-shutil.rmtree(test_path + "_clustered")
-print("Test passed, evidence destroyed.")
+        # create second Clustering object to merge clustered test images
+        clstr2 = Clustering(images_folder_path=test_path + "_clustered", method=method, num_of_threads=8, threshold=0.125,
+                        batch_size=50, overwrite="Y", transfer=transfer, verbose=1, scale=(1,1), option="merge")
+        clstr2()
+
+        shutil.rmtree(test_path)
+        shutil.rmtree(test_path + "_clustered")
+        print("Test passed, evidence destroyed.")
