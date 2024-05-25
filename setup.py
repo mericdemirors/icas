0df06295b30526e58d6f296c1265ad4a658f0868
mericdemirors <mericdemirors218@gmail.com>
2024-05-25 09:47:45 +0300

    path fix

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-with open("USAGINGandCONTRIBUTING.md", "r") as f:
+with open("USINGandCONTRIBUTING.md", "r") as f:
     usage_desc = f.read()
 
 setup(
