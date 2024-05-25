8f6e97497df35ef1741995940a92e0cc69a1664e
mericdemirors <mericdemirors218@gmail.com>
2024-05-25 11:02:50 +0300

    updated setup

--- setup.py
+++ setup.py
@@ -1,27 +1,27 @@
 from setuptools import setup, find_packages
 
-with open("USINGandCONTRIBUTING.md", "r") as f:
+with open("USAGE.md", "r") as f:
     usage_desc = f.read()
 
 setup(
     name="labelimg",
-    version="1.0",
+    version="1.2",
     packages=find_packages(),
     description="Tool for labeling images",
     install_requires=[
-        "numpy>=1.26.4",
-        "opencv_contrib_python>=4.9.0.80", # be careful about other opencv libraries on your device
-        "scikit-image>=0.22.0",
-        "tqdm>=4.66.2",
-        "matplotlib>=3.8.4",
-        "mplcursors>=0.5.3",
-        "datasketch>=1.6.4",
-        "Pillow>=10.3.0",
-        "ImageHash>=4.3.1",
-        # "torch>=2.3.0",
-        # "scikit-learn>=1.4.2",
-        # "torchvision>=0.18.0",
-        # "segment-anything>=1.0"
+        "numpy", #>=1.26.4",
+        "opencv_contrib_python", #>=4.9.0.80", # be careful about other opencv libraries on your device
+        "scikit-image", #>=0.22.0",
+        "tqdm", #>=4.66.2",
+        "matplotlib", #>=3.8.4",
+        "mplcursors", #>=0.5.3",
+        "datasketch", #>=1.6.4",
+        "Pillow", #>=10.3.0",
+        "ImageHash", #>=4.3.1",
+        # "torch", #>=2.3.0",
+        # "scikit-learn", #>=1.4.2",
+        # "torchvision", #>=0.18.0",
+        # "segment-anything", #>=1.0"
     ],
     long_description=usage_desc,
     long_description_content_type="text/markdown",
