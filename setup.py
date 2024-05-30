7cc3d8575197df7a5ccbc28f9c2122c0e673efb1
mericdemirors <mericdemirors218@gmail.com>
2024-05-30 15:24:26 +0300

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("USAGE.md", "r") as f:
 
 setup(
     name="icas",
-    version="1.0",
+    version="1.1",
     packages=find_packages(),
     description="Tool for labeling images",
     install_requires=[
